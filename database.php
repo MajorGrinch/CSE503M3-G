a302f9e9d375c3de65378045684f5a2f6dc917a3
ZIYANG WANG <wangziyang@wustl.edu>
2017-09-29 14:09:29 -0500

    rm database php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mysqli = new mysqli('127.0.0.1', 'root', 'Aptx4869!!', 'news');
-
-if ($mysqli->connect_errno) {
-    printf("Connection Failed: %s\n", $mysqli->connect_error);
-    exit;
-}
