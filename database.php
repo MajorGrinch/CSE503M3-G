a5e84d7ba99d64969576098a6f8f0526d02bdf86
ZIYANG WANG <wangziyang@wustl.edu>
2017-10-01 22:32:29 -0500

    update database file

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', 'toor', 'news');
+$mysqli = new mysqli('localhost', 'wustl_inst', 'wustl_pass', 'news');
 
 if ($mysqli->connect_errno) {
     printf("Connection Failed: %s\n", $mysqli->connect_error);
